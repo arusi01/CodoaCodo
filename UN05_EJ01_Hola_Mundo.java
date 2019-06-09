77306d09951a5e29b1ebe1dcd60e442ac8e93ccb
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-06-09 17:02:26 -0300

    Add files via upload

---
+++ UN05_EJ01_Hola_Mundo.java
@@ -0,0 +1,12 @@
+package cfp10;
+
+public class UN05_EJ01_Hola_Mundo {
+
+    public static void Hola() {
+        System.out.println("Hola Mundo");
+    }
+
+    public static void main(String[] args) {
+        Hola();
+    }
+}
