f5ba63c58802251253686f83bc4370efa52a55ec
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-06-18 12:29:49 -0300

    Add files via upload

---
+++ Junio18/JUN18_Matriz_Multipl.java
@@ -0,0 +1,33 @@
+package cfp10;
+
+import java.util.Scanner;
+
+public class JUN18_Matriz_Multipl {
+       public static void main(String[] args) {
+       
+        int FILA=3;
+        int COLUMNA=3;
+        int VECTOR[][] = new int[FILA][COLUMNA]; //arreglo de 3x3 de 3 filas y 3 columnas
+        System.out.println(" Arreglo de [" + FILA + "] x [" + COLUMNA + "]");
+        VECTOR[0][0] = 3;
+        VECTOR[0][1] = 4;
+        VECTOR[0][2] = 5;
+        VECTOR[1][0] = 6;
+        VECTOR[1][1] = 7;
+        VECTOR[1][2] = 8;
+        VECTOR[2][0] = 11;
+        VECTOR[2][1] = 20;
+        VECTOR[2][2] = 34;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       }
+       
+}
