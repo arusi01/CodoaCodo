4fb774febaf2856a8248c66f00db759c45f51e12
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-06-18 12:12:32 -0300

    Add files via upload

---
+++ Junio18/JUN18_Matriz_3x3.java
@@ -0,0 +1,31 @@
+package cfp10;
+
+import java.util.Scanner;
+
+public class JUN18_Matriz_3x3 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ingrese Filas Matriz: ");
+        int Fila = sc.nextInt();
+        System.out.print("Ingrese Columnas Matriz: ");
+        int Columna = sc.nextInt();
+
+        int vector[][] = new int[Fila][Columna];
+
+        for (int f = 0; f < Fila; f++) {
+            for (int c = 0; c < Columna; c++) {
+                System.out.print("Ingrese valor Fila " + f + " Columna " + c + ": ");
+                vector[f][c] = sc.nextInt();
+            }
+        }
+        
+        for (int f = 0; f < Fila; f++) {
+            for (int c = 0; c < Columna; c++) {
+                System.out.println("Valor Fila " + f + " Columna " + c + ": "+vector[f][c]);
+            }
+        }
+    }
+}
