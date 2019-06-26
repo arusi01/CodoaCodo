0c423b598bed8c5910907a5eb533ab4cad96d03c
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-06-26 20:06:05 -0300

    Update JUN18_Matriz_3x3.java

--- Junio18/JUN18_Matriz_3x3.java
+++ Junio18/JUN18_Matriz_3x3.java
@@ -13,18 +13,18 @@ public class JUN18_Matriz_3x3 {
         System.out.print("Ingrese Columnas Matriz: ");
         int Columna = sc.nextInt();
 
-        int vector[][] = new int[Fila][Columna];
+        int matriz[][] = new int[Fila][Columna];
 
         for (int f = 0; f < Fila; f++) {
             for (int c = 0; c < Columna; c++) {
                 System.out.print("Ingrese valor Fila " + f + " Columna " + c + ": ");
-                vector[f][c] = sc.nextInt();
+                matriz[f][c] = sc.nextInt();
             }
         }
         
         for (int f = 0; f < Fila; f++) {
             for (int c = 0; c < Columna; c++) {
-                System.out.println("Valor Fila " + f + " Columna " + c + ": "+vector[f][c]);
+                System.out.println("Valor Fila " + f + " Columna " + c + ": "+matriz[f][c]);
             }
         }
     }
