446781ec4a0c0f9aa2ef53f1e72f0978e483beb2
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-06-26 21:05:52 -0300

    Update JUN18_Matriz_Multipl.java

--- Junio18/JUN18_Matriz_Multipl.java
+++ Junio18/JUN18_Matriz_Multipl.java
@@ -3,31 +3,50 @@ package cfp10;
 import java.util.Scanner;
 
 public class JUN18_Matriz_Multipl {
-       public static void main(String[] args) {
-       
-        int FILA=3;
-        int COLUMNA=3;
-        int VECTOR[][] = new int[FILA][COLUMNA]; //arreglo de 3x3 de 3 filas y 3 columnas
-        System.out.println(" Arreglo de [" + FILA + "] x [" + COLUMNA + "]");
-        VECTOR[0][0] = 3;
-        VECTOR[0][1] = 4;
-        VECTOR[0][2] = 5;
-        VECTOR[1][0] = 6;
-        VECTOR[1][1] = 7;
-        VECTOR[1][2] = 8;
-        VECTOR[2][0] = 11;
-        VECTOR[2][1] = 20;
-        VECTOR[2][2] = 34;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-       }
-       
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int filas;
+        do {
+            System.out.print("Ingrese total de filas de la matriz: ");
+            filas = sc.nextInt();
+        } while (filas < 1);
+
+        int columnas;
+        do {
+            System.out.print("Ingrese total de columnas de la matriz: ");
+            columnas = sc.nextInt();
+        } while (columnas < 1);
+
+        int matriz[][] = new int[filas][columnas];
+
+        System.out.println("\nIngrese los elementos de la Matriz:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print("Posicion " + i + "," + j + ": ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("\nMatriz Ingresada:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("\nMultiplicacion de la diagonal:");
+        int resultado = matriz[0][0];
+        for (int i = 0; i < filas; i++) {
+            if (i == 0) {
+                System.out.print(matriz[i][i]);
+            } else {
+                resultado = resultado * matriz[i][i];
+                System.out.print(" * " + matriz[i][i]);
+            }
+        }
+        System.out.println(" = " + resultado);
+    }
 }
