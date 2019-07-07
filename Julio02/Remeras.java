0a48fcb88b6c8c0c134f603b2169cc138d664e2e
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-07-07 19:13:13 -0300

    Add files via upload

---
+++ Julio02/Remeras.java
@@ -0,0 +1,15 @@
+package JUN02_Remeras;
+
+public class Remeras {
+    
+    public String color;
+    public float precio;
+    public String talle;
+    public String descripcion;
+    
+    public void mostrar(){
+        System.out.println("Remera de color "+color+" talle "+talle+" y tipo "
+                +descripcion+" sale $ "+precio);
+    }
+    
+}
