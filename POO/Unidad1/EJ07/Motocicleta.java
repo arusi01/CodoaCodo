d695c7b755987ba52c93f7b77ebe1e8d5835575e
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-07-11 11:51:18 -0300

    Add files via upload

---
+++ POO/Unidad1/EJ07/Motocicleta.java
@@ -0,0 +1,17 @@
+package UN01_EJ07_Motocicleta;
+
+public class Motocicleta {
+    
+    private String Matricula;
+    private String Color;
+    private int Velocidad;
+    private boolean enMarcha;
+    
+    public Motocicleta(String Matricula, String Color, int Velocidad, boolean enMarcha) {
+        this.Matricula=Matricula;
+        this.Color=Color;
+        this.Velocidad=Velocidad;
+        this.enMarcha=enMarcha;
+    }
+    
+}
