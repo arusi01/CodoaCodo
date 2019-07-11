21c003a8041445cbe352f6b1405544ff8b638577
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-07-11 11:50:34 -0300

    Add files via upload

---
+++ POO/Unidad1/Main.java
@@ -0,0 +1,9 @@
+package UN01_EJ07_Motocicleta;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Motocicleta Yamaha = new Motocicleta("123ABC","Azul",100,false);
+    } 
+}
