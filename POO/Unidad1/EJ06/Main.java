5e71721bbc4196f0a979d348c38629fc3d47a986
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-07-11 11:36:44 -0300

    Add files via upload

---
+++ POO/Unidad1/EJ06/Main.java
@@ -0,0 +1,16 @@
+package UN01_EJ06_Gato;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Gato angora = new Gato("Blanquito","Asiatico","Blanco");
+        
+        angora.caminar();
+        angora.jugar();
+        angora.maullar();
+        angora.saltar();
+        
+    }
+    
+}
