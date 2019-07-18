6df11a5fe0089106272f7e5b23c70b5dd2e7768e
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-07-18 10:17:10 -0300

    Update Main.java

--- Julio18/ArrayList_Nombres/Main.java
+++ Julio18/ArrayList_Nombres/Main.java
@@ -4,20 +4,24 @@ import java.util.ArrayList;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         ArrayList<String> lista = new ArrayList<>();  // Dinamico
-                 
+
         lista.add("Alberto");
         lista.add("Carlos");
         lista.add("Pepe");
-        
-//      for  CTRL+SPACE
-        for(int i=0; i < lista.size(); i++){
+
+        System.out.println("Largo de la lista: " + lista.size() + "\n");
+
+        for (int i = 0; i < lista.size(); i++) {
             String datos = lista.get(i);
-            System.out.println("Nombre: "+datos);
+                 // Variable de referencia
+            System.out.println("Nombre: " + datos);
+            
+            if (lista.get(i).equals("Carlos")) {
+                System.out.println("Carlos Encontrado en posicion: "+i);
+            }
         }
     }
-    
 }
