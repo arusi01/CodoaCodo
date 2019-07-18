d571c5341e4b71e0c92fc013fd1188ca55add1c7
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-07-18 09:59:56 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package JUN18_List_ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int vector[] = new int [3];   // Vector Estatico
+        
+        List<String> list = new ArrayList<>();
+  
+        ArrayList<Integer> lista = new ArrayList<>();  // Dinamico
+               // Clase Integer
+                 
+        lista.add(2);
+        lista.add(5);
+        lista.add(7);
+        
+//      for  CTRL+SPACE
+        for(int i=0; i < lista.size(); i++){
+            Integer datos = lista.get(i);
+            System.out.println("N°: "+datos);
+        }
+    }
+    
+}
