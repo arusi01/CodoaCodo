1a2eaf9e94b0e990f50586b5ec9119879aaac07f
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-06 10:03:25 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package AGO08_Contructor_This;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona Carlos = new Persona("Carlos");
+        Persona2 Daniel = new Persona2("daniel");
+        
+        System.out.println(Carlos.toString());
+        System.out.println(Daniel.toString());
+    }
+    
+}
