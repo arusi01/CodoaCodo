cc21f20df79cea69e17a267b9129a80b73a1f7d7
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-06 10:06:51 -0300

    Add files via upload

---
+++ Agosto08/Constructor_This/Persona.java
@@ -0,0 +1,15 @@
+package AGO08_Contructor_This;
+
+public class Persona {
+    
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String toString() {
+        return "Persona1 " + nombre;
+    }
+    
+}
---
+++ Agosto08/Constructor_This/Persona2.java
@@ -0,0 +1,14 @@
+package AGO08_Contructor_This;
+
+public class Persona2 {
+    
+    private String nombre;
+
+    public Persona2(String nom) {
+        nombre = nom;
+    }
+    
+    public String toString() {
+        return "Persona2 " + nombre;
+    }
+}
