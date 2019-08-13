049198d3dc27f24c311fe18c804fbd8a29ac0e6c
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-13 09:18:01 -0300

    Add files via upload

---
+++ Agosto08/AGO08_ArrayListObjeto_Metodos/Persona.java
@@ -0,0 +1,59 @@
+package AGO08_ArrayListObjeto_Metodos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Persona {
+
+    //Atributos
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona() {
+    }
+
+    // Constructor con Parametros
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+    
+    public static void cargarLista(List<Persona> persona) {
+
+        for (int i = 0; i < 2; i++) {
+
+            Scanner teclado = new Scanner(System.in);
+
+            System.out.print("Ingrese su nombre: ");
+            String nombre = teclado.next();
+            System.out.print("Ingrese su apellido: ");
+            String apellido = teclado.next();
+            System.out.print("Ingrese su edad: ");
+            int edad = teclado.nextInt();
+
+            Persona pers = new Persona(nombre, apellido, edad);
+
+            //agregamos a la lista
+            persona.add(pers);
+        }
+    }
+    
+    public List<Persona> mostrar(List<Persona> persona) {
+          return persona;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
