8a53c1e8e9992d7556f34eefc47af22debcfeff9
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-15 09:26:07 -0300

    Update Biblioteca.java

--- Agosto13/AGO13_Biblioteca/Biblioteca.java
+++ Agosto13/AGO13_Biblioteca/Biblioteca.java
@@ -1,47 +1,127 @@
 package AGO13_Biblioteca;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Biblioteca {
 
-    public static void main(String[] args) {
+    //Atributos
+    private String nombreLibro;
+    private int codigoLibro;
+    private int nunEstante;
+    private int precioLibro;
+
+    public Biblioteca() {
+    }
+
+    public Biblioteca(String nombreLibro, int codigoLibro, int nunEstante, int precioLibro) {
+        this.nombreLibro = nombreLibro;
+        this.codigoLibro = codigoLibro;
+        this.nunEstante = nunEstante;
+        this.precioLibro=precioLibro;
+    }
+
+    public String getNombreLibro() {
+        return nombreLibro;
+    }
+
+    public void setNombreLibro(String nombreLibro) {
+        this.nombreLibro = nombreLibro;
+    }
+
+    public int getCodigoLibro() {
+        return codigoLibro;
+    }
+
+    public void setCodigoLibro(int codigoLibro) {
+        this.codigoLibro = codigoLibro;
+    }
+
+    public int getNunEstante() {
+        return nunEstante;
+    }
+
+    public void setNunEstante(int nunEstante) {
+        this.nunEstante = nunEstante;
+    }
+
+    public int getPrecioLibro() {
+        return precioLibro;
+    }
+
+    public void setPrecioLibro(int precioLibro) {
+        this.precioLibro = precioLibro;
+    }
+
+    public void cargarEstante(List<Biblioteca> libro) {
 
         Scanner teclado = new Scanner(System.in);
 
-        Biblioteca ejemplar = new Biblioteca();
+        System.out.print("\nIngrese el tamaño del arreglo: ");
+        int tamanio = teclado.nextInt();
 
-        ArrayList<Biblioteca> listaLibros = new ArrayList<>();
-        
-        ejemplar.cargarEstante(listaLibros);
-        
-        Object objMay=new Object();
-        objMay=ejemplar.mostrarMayorPrecio(listaLibros).getPrecioLibro();
-        System.out.println("\nLibro con mayor precio: "+objMay);
-        
-        System.out.println("\nIngrese el codigo del libro a eliminar: ");
-        int codLibro=teclado.nextInt();
-        ejemplar.eliminarLibro(listaLibros, codLibro);
-        
-        List<Biblioteca> mostrar = ejemplar.mostrarLibros(listaLibros);
-        for (int i = 0; i < mostrar.size(); i++) {
-            Biblioteca get = mostrar.get(i);
-            System.out.println("\nCodigo Libro: " + get.getCodigoLibro()
-                    + " N Estante: " + get.getNunEstante()
-                    + " Nombre Libro: " + get.getNombreLibro());
+        for (int i = 0; i < tamanio; i++) {
+
+            System.out.println("\nIngrese los datos del libro " + i);
+
+            System.out.print("Codigo ISBN: ");
+            int codigoLibros = teclado.nextInt();
+            System.out.print("Nombre: ");
+            String nombreLibros = teclado.next();
+            System.out.print("Estante: ");
+            int nunEstantes = teclado.nextInt();
+            System.out.print("Precio: ");
+            int precioLibro = teclado.nextInt();
+            
+            Biblioteca ejemplar = new Biblioteca(nombreLibros, codigoLibros, nunEstantes, precioLibro);
+
+            //agregamos a la lista
+            libro.add(ejemplar);
+        }  
+    }       
+
+    public List<Biblioteca> eliminarLibro(List<Biblioteca> libro, int codigo) {
+        for (int i = 0; i < libro.size(); i++) {
+            if (libro.get(i).getCodigoLibro()==codigo) {
+                libro.remove(i);
+            }
         }
-        
-        System.out.println("\nIngrese el codigo del libro a buscar: ");
-        int codLibroBuscar=teclado.nextInt();
-        ejemplar.encontrarLibro(listaLibros, codLibroBuscar);
-        
-        System.out.println("\nIngrese el codigo del libro a buscar2: ");
-        int codLibroBuscar2 = teclado.nextInt();
-        if (ejemplar.encontrarLibro2(listaLibros, codLibroBuscar)) {
-            System.out.println("\nLibro Encontrado.");
-        } else {
-            System.out.println("\nLibro NOOOO Encontrado.");
+        return libro;
+    }
+
+    public List<Biblioteca> mostrarLibros(List<Biblioteca> libro) {
+        return libro;
+    }
+
+    public List<Biblioteca> encontrarLibro(List<Biblioteca> libro, int codigo) {
+        for (int i = 0; i < libro.size(); i++) {
+            if (libro.get(i).getCodigoLibro() == codigo) {
+                 System.out.println("\nLibro Encontrado.    1");
+            }else{
+                 System.out.println("\nLibro NOOO Encontrado.   1");
+
+            }
         }
+        return libro;
+    }
+    
+    public boolean encontrarLibro2(List<Biblioteca> libro, int codigo) {
+        boolean encontrado = false;
+        for (int i = 0; i < libro.size(); i++) {
+            if (libro.get(i).getCodigoLibro() == codigo) {
+                encontrado=true;
+            }
+        }
+        return (encontrado);
+    }
+    
+    public Biblioteca mostrarMayorPrecio(List<Biblioteca> libro) {
+        Biblioteca mayor = libro.get(0);
+        for (int i = 0; i < libro.size(); i++) {
+            if (libro.get(i).getPrecioLibro()>mayor.getPrecioLibro()) {
+                mayor=libro.get(i);
+            }
+        }
+        return mayor;        
     }
 }
