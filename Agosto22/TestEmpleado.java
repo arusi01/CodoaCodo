5f51142ecaa559555259fb1e017232030cff261a
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-22 11:02:42 -0300

    Add files via upload

---
+++ Agosto22/TestEmpleado.java
@@ -0,0 +1,13 @@
+package AGO22_Empleados;
+
+public class TestEmpleado {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Empleados emps = new Empleados();
+        for (Object emp : emps) {
+            System.out.println(emp);
+        }
+    }
+    
+}
