4df72213986fb32f2c5954cf3efacc1766ea6a8c
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-22 11:24:28 -0300

    Add files via upload

---
+++ Agosto22/Persona.java
@@ -0,0 +1,29 @@
+package AGO22_Persona;
+
+public class Persona {
+
+    private String nombre;
+    private int dni;
+
+    public Persona(String nombre, int dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+}
