c1a4ce1d8132a95a972734d96e134ba7e81ff778
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-27 09:41:37 -0300

    Add files via upload

---
+++ Agosto22/Main.java
@@ -0,0 +1,15 @@
+package AGO27_Enum;
+
+enum Color
+{
+    ROJO, VERDE, AZUL;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Color c1 = Color.ROJO;
+        System.out.println(c1);
+    }
+}
