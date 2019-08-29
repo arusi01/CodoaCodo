169f3f084b43bbc0df88f780d6f8a1c00f306c1b
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-29 11:04:54 -0300

    Add files via upload

---
+++ Agosto29/Main.java
@@ -0,0 +1,17 @@
+package AGO29_Aerosub2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Aerosub avion = new Aerosub();
+        avion.Despegar();
+        avion.Acuatizar();
+        
+        Aerosub barco = new Aerosub();
+        barco.Sumergir();
+        
+    }
+    
+}
