662ea7ec7b19eff744c330ca9676559e4901449f
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-08-29 10:20:51 -0300

    Add files via upload

---
+++ Agosto29/Main.java
@@ -0,0 +1,17 @@
+package AGO29_Interfaces_Persona;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Alumno a = new Alumno();
+        a.ingresar();
+        a.mostrar();
+        
+        Persona b = new Alumno();
+        b.ingresar();
+        b.mostrar();
+    }
+    
+}
