0473785d69ad22a888b3e24bf8b8151b430713a3
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-09-19 09:11:30 -0300

    Add files via upload

---
+++ Septiembre17/SEP17_FrameDemo/JButton.java
@@ -0,0 +1,12 @@
+package SEP17_FrameDemo;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JButton {
+    
+    JLabel emptyLabel = new JLabel("Prueba");
+    JFrame frame = new JFrame("FrameDemo");
+
+      btnSumar.a    
+}
