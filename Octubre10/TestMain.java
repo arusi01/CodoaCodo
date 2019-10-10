fd0c6bd305e8fd2e4a4fd8999130509c3c3b9a72
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-10-10 11:05:15 -0300

    Add files via upload

---
+++ Octubre10/TestMain.java
@@ -0,0 +1,11 @@
+package OCT10_Producto;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vista v = new Vista();
+        v.setVisible(true);
+    }
+    
+}
