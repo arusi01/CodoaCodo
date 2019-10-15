72cfe9ad7cbb6da9ba3dc2e6ba21bae100f20f7a
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-10-15 10:56:51 -0300

    Add files via upload

---
+++ Octubre10/StartPage.java
@@ -0,0 +1,14 @@
+package OCT15_Ventas;
+
+public class StartPage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana_Ventas v = new Ventana_Ventas();
+        v.setVisible(true);
+    }
+    
+}
