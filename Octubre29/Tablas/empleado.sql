deb555b8ae90cc5b11f0e5dcbbb99623ae1df51c
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-10-29 11:06:38 -0300

    Update empleado.sql

--- Octubre29/Tablas/empleado.sql
+++ Octubre29/Tablas/empleado.sql
@@ -1,5 +1,5 @@
 -- crear una base de datos tipo persona que contenga tres tablas empleado, productos, administradores
--- en la tabla empleado colocar un id tipo int not null primary key y con auto incremento
+-- en la tabla empleado colocar un id_tipo int not null primary key y con auto incremento
 -- otra columna nombre varchar not null, luego apellido varchar not null 
 -- area de trabajo varchar not null, titulo varchar not null
 -- segunda tabla productos id_producto int not null primary key y con auto incremento
@@ -9,4 +9,14 @@
 -- telefono_adminstrador varchar not null, correo_administrador varchar not null
 -- relacionar las dos tablas con foreign key (empleado y productos)
 
+create database persona;
+use persona;
+
+create table empleado(
+id_tipo int auto_increment primary key,
+nombre varchar(45) not null,
+apellido varchar(45) not null,
+area_trabajo varchar(45) not null,
+titulo varchar(45) not null
+);
 
