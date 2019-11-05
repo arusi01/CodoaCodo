a7fe713bcd182fb39e194e55946201db83faf6a1
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-11-05 11:10:25 -0300

    Update Conexion.java

--- Noviembre05/Mysql/Conexion.java
+++ Noviembre05/Mysql/Conexion.java
@@ -19,9 +19,31 @@ public class Conexion {
             con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/bd", "root", "");
             Statement st = (Statement) con.createStatement();
             System.out.println("coneccion exitosa");
+
+            // Insertar
             String sql = "insert into usuario(nombre,apellido,edad,sexo) "
                     + "values ('Tomi','Montana','38','M')";
             st.execute(sql);
+
+            st.execute("insert into usuario(nombre,apellido,edad,sexo) "
+                    + "values ('Alberto','Perez','35','M')");
+            
+            // Seleccionar
+            String sql1= "select * from usuario";
+            ResultSet rs = st.executeQuery(sql1);
+            while (rs.next()) {
+                System.out.println("Nombre: "+rs.getString("nombre"));
+                System.out.println("Apellido: "+rs.getString("apellido"));
+                System.out.println("Edad: "+rs.getString("edad"));
+                System.out.println("Sexo: "+rs.getString("sexo"));
+                System.out.println("");                 
+            }
+
+            // Borrar
+            String sql2= "delete from usuario where nombre='Tomi';";
+            st.execute(sql2);
+
+            
         } catch (SQLException ex) {
             Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);   
         }
