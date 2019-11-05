64b6d4fba126a32c818f14fbcbace7115876feac
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-11-05 10:56:29 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package NOV05_BD_POO;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Conexion con = new Conexion();
+        try {
+            con.obtener();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex); 
+        }   
+    }
+}
