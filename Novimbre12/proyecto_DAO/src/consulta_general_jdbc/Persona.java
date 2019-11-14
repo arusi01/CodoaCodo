cfad871b3914661efadfa2b23e4939e5d3a6bbea
arusi01 <51458416+arusi01@users.noreply.github.com>
2019-11-14 09:40:35 -0300

    Add files via upload

---
+++ Novimbre12/proyecto_DAO/src/consulta_general_jdbc/Persona.java
@@ -0,0 +1,36 @@
+package consulta_general_jdbc;
+
+public class Persona {
+    
+    public int id;
+    public String nombre;
+    public String apellido;
+
+    public Persona() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+}
